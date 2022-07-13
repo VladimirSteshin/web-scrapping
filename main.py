f13b6fbfd34bdd8f75349e626b1ec53424b90769
Steshin <@gs.pmok>
2022-07-13 18:36:32 +0300

    Trying different func

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import requests
 from fake_headers import Headers
 
 
-def get_url(file):
+def get_url_and_keywords(file):
     config = configparser.ConfigParser()
-    config.read(file)
+    config.read(file, encoding='UTF-8')
     URL = config['URL']['url']
-    return URL
+    KEYWORDS = config['KEYWORDS']['keywords']
+    KEYWORDS = KEYWORDS.split(', ')
+    return URL, KEYWORDS
 
 
 def get_headers():
@@ -17,9 +19,27 @@ def get_headers():
     return HEADERS
 
 
+def get_response(path, params):
+    response = requests.get(path, headers=params)
+    data = response.text
+    return data
+
+
+def get_articles(data):
+    soup = bs4.BeautifulSoup(data, features='html.parser')
+    item = soup.find_all('article')
+    return item
+
+
+def find_coincidences(articles_found, keywords):
+    for article in articles_found:
+        preview = article.find_all(class_='tm-article-body tm-article-snippet__lead')
+        print()
+
+
 if __name__ == "__main__":
-    url = get_url('config.ini')
+    url, keywords = get_url_and_keywords('config.ini')
     headers = get_headers()
-    print(url)
-    print(headers)
-    
+    text = get_response(url, headers)
+    articles = get_articles(text)
+    result = find_coincidences(articles, keywords)
