0c6992e379ce52d1b4b4ad51a93ff9a44f6c3e13
Steshin <@gs.pmok>
2022-07-13 17:17:04 +0300

    Got configs and headers

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import bs4
+import configparser
+import requests
+from fake_headers import Headers
 
+
+def get_url(file):
+    config = configparser.ConfigParser()
+    config.read(file)
+    URL = config['URL']['url']
+    return URL
+
+
+def get_headers():
+    HEADERS = Headers(headers=True)
+    HEADERS = HEADERS.generate()
+    return HEADERS
+
+
+if __name__ == "__main__":
+    url = get_url('config.ini')
+    headers = get_headers()
+    print(url)
+    print(headers)
+    
