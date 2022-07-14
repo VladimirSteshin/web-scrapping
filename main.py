0f75d05b4e6a32c253038f2afd54f3102971c98f
Steshin <@gs.pmok>
2022-07-14 13:04:00 +0300

    Done

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ from fake_headers import Headers
 def get_url_and_keywords(file):
     config = configparser.ConfigParser()
     config.read(file, encoding='UTF-8')
-    URL = config['URL']['url']
+    BASE_URL = config['URL']['base_url']
+    WORK_URL = config['URL']['work_url']
     KEYWORDS = config['KEYWORDS']['keywords']
     KEYWORDS = KEYWORDS.split(', ')
-    return URL, KEYWORDS
+    return BASE_URL, WORK_URL, KEYWORDS
 
 
 def get_headers():
@@ -25,16 +26,24 @@ def get_response(path, params):
     return data
 
 
-def get_articles(data, check):
+def get_articles(data, check, path):
     soup = bs4.BeautifulSoup(data, features='html.parser')
     articles = soup.find_all('article')
     for article in articles:
         preview = article.find_all(class_='article-formatted-body')
-        print(preview)
+        for info in preview:
+            data = info.text.lower()
+            for word in check:
+                if word.lower() in data:
+                    found_time = article.find('time').attrs['title'].split(', ')[0]
+                    found_name = article.find(class_='tm-article-snippet__title-link').text
+                    found_href = article.find(class_='tm-article-snippet__title-link').attrs['href']
+                    print(f'{found_time} {found_name} {path + found_href}')
+                    break
 
 
 if __name__ == "__main__":
-    url, keywords = get_url_and_keywords('config.ini')
+    base_url, work_url, keywords = get_url_and_keywords('config.ini')
     headers = get_headers()
-    text = get_response(url, headers)
-    articles_found = get_articles(text, keywords)
+    text = get_response(work_url, headers)
+    get_articles(text, keywords, base_url)
